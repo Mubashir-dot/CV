8366148b9b42e2fc2ce606b2f6473ef1bee5b4b3
mubashir-dot <mmubimemon@gmail.com>
2019-12-24 20:48:50 +0500

    Alert concept of js

---
+++ javascript/index.js
@@ -0,0 +1,12 @@
+// //alert("hello!");
+// var user = "mubi";
+// alert(user)
+// //user 
+// //alert(user)
+user = "muneeb";
+alert(user);
+console.log(user)
+var quote = '"how you doing"'
+console.log(quote)
+var quote = "your's sincerely"
+console.log(quote)
