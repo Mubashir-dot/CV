ecb84ce70e0b41e80e6cdc1f97dad0bc0421356d
mubashir-dot <mmubimemon@gmail.com>
2020-01-24 17:39:33 +0500

    javascript in practice

---
+++ javascript/edureka/index.js
@@ -0,0 +1,32 @@
+// dataTypes in JS
+//var me = true;
+//console.log(typeof me)
+
+// var weight = parseFloat(prompt('enter your weight !'))
+// switch(weight)
+
+// var subjects = ['Physics', 'Chemistry', 'Maths']
+// var marks = new Array();
+// var num1;
+
+// for(var m = 0; m < subjects.length; m++){
+//     num1 = parseFloat(prompt('enter marks for subject ' + subjects[m]));
+//     marks[m] = num1;
+//     //console.log(marks);
+// }
+// for(n in marks){
+//     console.log(marks[n]);
+// }
+// var i = 8;
+// while(i < 10){
+//     console.log(i);
+//     i++
+// }
+
+
+
+
+for (let i = 5; i >= 1; i-- ){
+    if (i % 2 !== 0)
+    console.log(i);
+}
---
+++ javascript/rock paper scissors/index.js
@@ -0,0 +1,61 @@
+let userScore = 0;
+let computerScore = 0;
+const userScore_span = document.getElementById('user-score');
+const computerScore_span = document.getElementById('computer-score');
+const scoreBoard_div = document.querySelector('.score-board');
+const result_div = document.querySelector('.result');
+const rock_div = document.getElementById('r');
+const paper_div = document.getElementById('p');
+const scissor_div = document.getElementById('s');
+function getComputerChoice(){
+    const choices = ['r', 'p', 's'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+function win(user, computer) {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+
+}
+function lose() {
+    console.log('LOST');
+}
+function draw() {
+    console.log('DRAW');
+}
+
+function game(userChoice){
+    const computerChoice = getComputerChoice()
+    switch (userChoice + computerChoice){
+        case 'rs':
+        case 'pr':
+        case 'sp':
+            win();
+            break;
+        case 'rp':
+        case 'ps':
+        case 'sr':
+            lost();
+            break;
+            
+        case 'rr':
+        case 'pp':
+        case 'ss':
+            draw();
+            break;
+    }
+}
+
+function main(){
+rock_div.addEventListener('click', function(){
+    game('r');
+})
+paper_div.addEventListener('click', function(){
+    game('p');
+})
+scissor_div.addEventListener('click', function(){
+    game('s');
+})
+}
+main();
